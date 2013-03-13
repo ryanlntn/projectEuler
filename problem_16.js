324972683312257f2e5684961aa07567eb756d4e
Ryan Linton <ryanlntn@gmail.com>
2013-03-13 08:57:30 -0700

    added 16 as well

---
+++ problem_16.js
@@ -0,0 +1,27 @@
+// Power digit sum
+// Problem 16
+// 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+// What is the sum of the digits of the number 2^1000?
+
+
+// code I found on stackoverflow to allow big ints in javascript
+function toFixed(x) {
+  if (Math.abs(x) < 1.0) {
+    var e = parseInt(x.toString().split('e-')[1]);
+    if (e) {
+        x *= Math.pow(10,e-1);
+        x = '0.' + (new Array(e)).join('0') + x.toString().substring(2);
+    }
+  } else {
+    var e = parseInt(x.toString().split('+')[1]);
+    if (e > 20) {
+        e -= 20;
+        x /= Math.pow(10,e);
+        x += (new Array(e+1)).join('0');
+    }
+  }
+  return x;
+}
+
+var answer = toFixed(Math.pow(2, 1000));
