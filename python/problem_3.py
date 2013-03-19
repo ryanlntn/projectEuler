d48a94b7514c5d4f552092d7973c260382af381a
Ryan Linton <ryanlntn@gmail.com>
2013-03-19 09:03:38 -0700

    solve problem 3 in python and ruby

--- python/problem_3.py
+++ python/problem_3.py
@@ -3,3 +3,30 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 
 # What is the largest prime factor of the number 600851475143 ?
+
+def prime_factors(n):
+
+  factors = []
+
+  while n % 2 == 0:
+    factors.append(2)
+    n /= 2
+
+  if n == 1:
+    return factors
+
+  factor = 3
+
+  while factor * factor <= n:
+    if n % factor == 0:
+      factors.append(factor)
+      n /= factor
+    else:
+      factor += 2
+
+  factors.append(n)
+
+  return factors
+
+
+print max(prime_factors(600851475143))
--- ruby/problem_3.rb
+++ ruby/problem_3.rb
@@ -3,3 +3,35 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 
 # What is the largest prime factor of the number 600851475143 ?
+
+def prime_factors(n)
+
+  factors = []
+
+  while n % 2 == 0 do
+    factors.push(2)
+    n /= 2
+  end
+
+  if n == 1
+    return factors
+  end
+
+  factor = 3
+
+  while factor * factor <= n do
+    if n % factor == 0
+      factors.push(factor)
+      n /= factor
+    else
+      factor += 2
+    end
+  end
+
+  factors.push(n)
+
+  return factors
+
+end
+
+puts prime_factors(600851475143).max
