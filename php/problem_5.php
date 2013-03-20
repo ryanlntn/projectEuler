7339daaf292ecf38419a4e08637e7b67ccede41e
Ryan Linton <ryanlntn@gmail.com>
2013-03-19 17:41:58 -0700

    solved problem 5 in php

--- php/problem_5.php
+++ php/problem_5.php
@@ -5,5 +5,22 @@
 
 // What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
+function isDivisibleBy($n, $divisor) {
+  return $n % $divisor == 0;
+}
+
+$i = 20;
+$n = 20;
+
+while ($i) {
+  for ($i = 20; $i >= 1; $i--) {
+    if (!isDivisibleBy($n, $i)) {
+      $n += 20;
+      $i = 20;
+    }
+  }
+}
+
+echo $n;
 
 ?>
