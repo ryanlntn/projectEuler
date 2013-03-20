0e03ce40f842b269a39c0cfa9047b32f5bdfc445
Ryan Linton <ryanlntn@gmail.com>
2013-03-19 17:22:16 -0700

    solved problem 3 in php

--- php/problem_3.php
+++ php/problem_3.php
@@ -5,4 +5,28 @@
 
 // What is the largest prime factor of the number 600851475143 ?
 
+function factors($n) {
+  $fs = [];
+  while ($n % 2 === 0) {
+    array_push($fs, 2);
+    $n /= 2;
+  }
+  if ($n === 1) {
+    return $fs;
+  }
+  $f = 3;
+  while ($f * $f <= $n) {
+    if ($n % $f == 0) {
+      array_push($fs, $f);
+      $n /= $f;
+    } else {
+      $f += 2;
+    }
+  }
+  array_push($fs, $n);
+  return $fs;
+}
+
+echo max(factors(600851475143));
+
 ?>
