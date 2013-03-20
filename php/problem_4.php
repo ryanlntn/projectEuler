9d4aa43a74278172543a77767a837047c9463bf9
Ryan Linton <ryanlntn@gmail.com>
2013-03-20 11:58:27 -0700

    solved problem 4 in php

--- php/problem_4.php
+++ php/problem_4.php
@@ -5,4 +5,19 @@
 
 // Find the largest palindrome made from the product of two 3-digit numbers.
 
+$largest = 0;
+
+for ($i = 999; $i > 0; $i--) {
+  for ($j = 999; $i > 0; $j--) {
+    if ((string)($i * $j) == strrev((string)($i * $j))) {
+      if ($i * $j > $largest) {
+        $largest = $i * $j;
+      }
+      break;
+    }
+  }
+}
+
+echo $largest;
+
 ?>
