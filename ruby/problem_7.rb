b80d03a896436ff99ac3c6da3c8c6683e668feb2
Ryan Linton <ryanlntn@gmail.com>
2013-03-24 00:26:42 -0700

    solved problem 7 in ruby

--- ruby/problem_7.rb
+++ ruby/problem_7.rb
@@ -2,4 +2,18 @@
 # Problem 7
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
-# What is the 10 001st prime number?
+# What is the 10001st prime number?
+
+require 'mathn'
+
+primes = [2]
+n = 3
+
+while primes.length < 10002 do
+  if n.prime?
+    primes.push(n)
+  end
+  n += 2
+end
+
+puts primes[10000]
