23a72f94640713dace30af9818708b3ca5d16827
Ryan Linton <ryanlntn@gmail.com>
2013-03-28 08:33:18 -0700

    solved problem 10 with PHP

--- php/problem_10.php
+++ php/problem_10.php
@@ -5,4 +5,27 @@
 
 // Find the sum of all the primes below two million.
 
+function sieve($max) {                         // So I looked up prime generation and found out about the Sieve of Eratosthenes.
+  $D = []; 
+  $primes = [];                    // I'm not yet entirely sure how it works but it generates a list of primes up to
+  for ($q=2; $q<$max; $q++) {                 // a given max.
+    if ($D[$q]) {
+      for ($i=0; $i<count($D[$q]); $i++) {
+        $p = $D[$q][$i];
+        if ($D[$p+$q]) array_push($D[$p+$q],$p);
+        else $D[$p+$q]=[$p];
+      }
+      unset($D[$q]);
+    } else {
+      array_push($primes,$q);
+      if ($q*$q<$max) $D[$q*$q]=[$q];
+    }
+  }
+  return $primes;
+}
+
+$answer = array_sum(sieve(2000000));
+
+echo $answer;
+
 ?>
