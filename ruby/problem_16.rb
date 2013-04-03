6bca4dcf70e14a5d25ec774a7d059d050fe3d02d
Ryan Linton <ryanlntn@gmail.com>
2013-04-03 14:50:24 -0700

    solved problem 16 in ruby

--- ruby/problem_16.rb
+++ ruby/problem_16.rb
@@ -3,3 +3,10 @@
 # 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
 # What is the sum of the digits of the number 21000?
+
+num = 2**1000
+total = 0
+
+num.to_s.each_char { |n| total += n.to_i }
+
+puts total
