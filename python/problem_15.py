ad69b7187f96a1c79c609ed6ef0d879221c03eb3
Ryan Linton <ryanlntn@gmail.com>
2013-04-03 14:07:08 -0700

    solved problem 15 in ruby

--- python/problem_15.py
+++ python/problem_15.py
@@ -4,3 +4,16 @@
 # there are exactly 6 routes to the bottom right corner.
 
 # How many such routes are there through a 20 x 20 grid?
+
+import itertools
+
+def lattice_paths(x, y):
+  # Build lattice from dimension arguments
+  cols = [ 'R' for i in range(x) ]
+  rows = [ 'D' for i in range(y) ]
+  lattice = cols + rows
+  
+  # Return the number of unique permutations
+  return len(set(itertools.permutations(lattice)))
+
+print lattice_paths(20, 20)
--- ruby/problem_15.rb
+++ ruby/problem_15.rb
@@ -4,3 +4,13 @@
 # there are exactly 6 routes to the bottom right corner.
 
 # How many such routes are there through a 20 x 20 grid?
+
+def factorial n
+  n.downto(1).reduce(:*)
+end
+
+def lattice_paths x, y
+  factorial(x + y) / (factorial(x) * factorial(y))
+end
+
+puts lattice_paths 20, 20
