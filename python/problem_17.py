0da1e428d381663441a42f9e7479fe0173b4d893
Ryan Linton <ryanlntn@gmail.com>
2013-04-04 14:51:20 -0700

    solved problem 17 in python

--- python/problem_17.py
+++ python/problem_17.py
@@ -9,3 +9,53 @@
 # NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) 
 # contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" 
 # when writing out numbers is in compliance with British usage.
+
+singles = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+massive_string = ''
+
+# Count through the singles (i.e. 'one'-'nine')
+for single in singles:
+  massive_string += single
+  print single
+
+# Count through the 'teens' (i.e. 'ten'-'nineteen')
+for teen in teens:
+  massive_string += teen
+  print teen
+
+# Count the rest of the way to 'ninetynine'
+for ten in tens:
+  massive_string += ten
+  print ten
+  for single in singles:
+    massive_string += ten + single
+    print ten + single
+
+# Count the rest of the way to 'ninehundredninetynine'
+for single in singles:
+  massive_string += single + 'hundred'
+  print single + 'hundred'
+
+  for sing in singles:
+    massive_string += single + 'hundredand' + sing
+    print single + 'hundredand' + sing
+  
+  for teen in teens:
+    massive_string += single + 'hundredand' + teen
+    print single + 'hundredand' + teen
+  
+  for ten in tens:
+    massive_string += single + 'hundredand' + ten
+    print single + 'hundredand' + ten
+    
+    for sing in singles:
+      massive_string += single + 'hundredand' + ten + sing
+      print single + 'hundredand' + ten + sing
+
+massive_string += 'onethousand'
+
+print massive_string
+print len(massive_string)
