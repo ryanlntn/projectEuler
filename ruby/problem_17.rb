8b71b3a495a9517958a4e025da1ae9ebb1779676
Ryan Linton <ryanlntn@gmail.com>
2013-04-04 15:08:41 -0700

    solved problem 17 in ruby

--- ruby/problem_17.rb
+++ ruby/problem_17.rb
@@ -9,3 +9,53 @@
 # NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) 
 # contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" 
 # when writing out numbers is in compliance with British usage.
+
+singles = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+massive_string = ''
+
+# Count through the singles (i.e. 'one'-'nine')
+singles.each do |single|
+  massive_string += single
+end
+
+# Count through the 'teens' (i.e. 'ten'-'nineteen')
+teens.each do |teen|
+  massive_string += teen
+end
+
+# Count the rest of the way to 'ninetynine'
+tens.each do |ten|
+  massive_string += ten
+  singles.each do |single|
+    massive_string += ten + single
+  end
+end
+
+# Count the rest of the way to 'ninehundredninetynine'
+singles.each do |single|
+  massive_string += single + 'hundred'
+
+  singles.each do |sing|
+    massive_string += single + 'hundredand' + sing
+  end
+
+  teens.each do |teen|
+    massive_string += single + 'hundredand' + teen
+  end
+  
+  tens.each do |ten|
+    massive_string += single + 'hundredand' + ten
+    
+    singles.each do |sing|
+      massive_string += single + 'hundredand' + ten + sing
+    end
+  end
+end
+
+massive_string += 'onethousand'
+
+puts massive_string
+puts massive_string.length
