19fbb6ad0e220aae571ae20653c22eadcf3a54eb
Ryan Linton <ryanlntn@gmail.com>
2013-04-08 12:06:25 -0700

    solved problem 19 in javascript

--- javascript/problem_19.js
+++ javascript/problem_19.js
@@ -11,3 +11,16 @@
 // And on leap years, twenty-nine.
 // A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
 // How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+
+"use strict";
+
+var start_date = new Date(1901, 0, 1);
+var end_date = new Date(2000, 11, 31);
+var sundays_on_1st = 0;
+
+for (var date = start_date; date <= end_date; date.setDate(date.getDate() + 1)) {
+  if (date.getDate() === 1 && date.getDay() === 0)
+    sundays_on_1st++;
+}
+
+var answer = sundays_on_1st;
