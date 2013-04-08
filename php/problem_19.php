2eeb47ca0cc13aa230c23f1cf32c823548f5c684
Ryan Linton <ryanlntn@gmail.com>
2013-04-08 11:56:27 -0700

    solved problem 19 in php. not a fan.

--- php/problem_19.php
+++ php/problem_19.php
@@ -13,4 +13,18 @@
 // A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
 // How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
 
+$date_arr = array(1,1,1901);
+$end_date = "20001231";
+$sundays_on_1st = 0;
+
+do {
+  $check_date = gmdate('Ymd', gmmktime(0,0,0,$date_arr[1],$date_arr[0]++,$date_arr[2]));
+  if (gmdate('d', gmmktime(0,0,0,$date_arr[1],$date_arr[0],$date_arr[2])) == '01' && 
+      gmdate('l', gmmktime(0,0,0,$date_arr[1],$date_arr[0],$date_arr[2])) == 'Sunday') {
+    $sundays_on_1st += 1;
+  }
+} while($end_date != $check_date);
+
+echo $sundays_on_1st;
+
 ?>
