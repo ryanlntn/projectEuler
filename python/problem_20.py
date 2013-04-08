ef34d00251a5aa8b5716009c9eb39af92ebc1cfa
Ryan Linton <ryanlntn@gmail.com>
2013-04-08 13:36:49 -0700

    solved problem 20 in python. almost as neat as ruby

--- python/problem_20.py
+++ python/problem_20.py
@@ -6,3 +6,9 @@
 # and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 
 # Find the sum of the digits in the number 100!
+
+from math import factorial
+
+digits = list(str(factorial(100)))
+
+print reduce(lambda x, y: x + y, map(lambda x: int(x), digits))
