259bdee2eceaed39d63ae94580a189c182c806d4
Ryan Linton <ryanlntn@gmail.com>
2013-05-19 09:29:10 -0700

    solved problem 22 in ruby

--- ruby/problem_22.rb
+++ ruby/problem_22.rb
@@ -8,6 +8,28 @@
 
 # For example, when the list is sorted into alphabetical order, COLIN, which is worth
 # 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a
-# score of 938  53 = 49714.
+# score of 938 * 53 = 49714.
 
 # What is the total of all the name scores in the file?
+
+names = File.open("names.txt", "rb").read
+sorted_names = names.gsub('"', '').split(',').sort
+
+class String
+  def alpha_val
+    alpha_vals = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8,
+                   i: 9, j: 10, k: 11, l: 12, m: 13, n: 14, o: 15,
+                   p: 16, q: 17, r: 18, s: 19, t: 20, u: 21, v: 22,
+                   w: 23, x: 24, y: 25, z: 26 }
+
+    alpha_vals[self.downcase.to_sym]
+  end
+end
+
+total = 0
+
+sorted_names.each_with_index do |name, index|
+  total += (index + 1) * name.split('').map{ |char| char.alpha_val }.reduce(:+)
+end
+
+puts total
